a648adb2e784f68f338de65c193f9af092b4dc85
Jasper Hartong <jasperhartong@gmail.com>
2022-10-19 11:20:12 +0200

    Remove Blitz wrappers next config and _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,8 @@
-import { ErrorFallbackProps, ErrorComponent, ErrorBoundary, AppProps } from "@blitzjs/next"
-import React from "react"
-import { withBlitz } from "app/blitz-client"
-
-function RootErrorFallback({ error }: ErrorFallbackProps) {
-  return (
-    <ErrorComponent
-      statusCode={(error as any)?.statusCode || 400}
-      title={error.message || error.name}
-    />
-  )
-}
+import { AppProps } from "@blitzjs/next"
 
 function MyApp({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
-  return (
-    <ErrorBoundary FallbackComponent={RootErrorFallback}>
-      {getLayout(<Component {...pageProps} />)}
-    </ErrorBoundary>
-  )
+  return <>{getLayout(<Component {...pageProps} />)}</>
 }
 
-export default withBlitz(MyApp)
+export default MyApp
